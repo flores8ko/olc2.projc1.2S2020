3bfe98b7818e2b18171f2aa041254f570f89f42d
Fernando Flores <nbafernandoflores8@gmail.com>
2020-08-21 12:11:25 -0600

    feat (./src/utils/Utils.ts): DefaultValue function add

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,3 +1,6 @@
+import {NULL, UNDEFINIED} from "./PrimitiveTypoContainer";
+import {Cntnr} from "./Cntnr";
+
 export class SemanticException extends Error {
     constructor(message?: string) {
         super(message);
@@ -7,5 +10,14 @@ export class SemanticException extends Error {
 export class ErrorCompo extends Error {
     constructor(message?: string) {
         super(message);
+    }
+}
+
+export function DefaultValue(typo: string) : Cntnr {
+    switch (typo.toUpperCase()) {
+        case "NULL":
+            return new NULL();
+        default:
+            return new UNDEFINIED();
     }
 }
