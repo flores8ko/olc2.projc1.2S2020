1cdd293d363f10663de1810a10a647c375024d26
Fernando Flores <nbafernandoflores8@gmail.com>
2020-08-24 11:27:55 -0600

    feat: Declare var list node structure add

---
+++ src/nodes/DeclareVarListNode.ts
@@ -0,0 +1,31 @@
+import {Op} from "../utils/Op";
+import {Envmnt} from "../utils/Envmnt";
+import {DeclareVarNode} from "./DeclareVarNode";
+import {Cntnr} from "../utils/Cntnr";
+
+export class DeclareVarListNode extends Op {
+    private readonly id: string;
+    private readonly value: Op;
+    private readonly declarationOps: Array<Op>;
+
+    constructor(id: string, declarationOps: Array<Op>, value?: Op) {
+        super();
+        this.id = id;
+        this.declarationOps = declarationOps;
+        this.value = value || null;
+    }
+
+    GO(env: Envmnt): object {
+        for (let op of this.declarationOps) {
+            try {
+                if (this.value !== null) {
+                    (op as DeclareVarNode).AddValue(this.value.Exe(env) as Cntnr);
+                }
+                op.Exe(env);
+            } catch (e) {
+                e.message();
+            }
+        }
+        return null;
+    }
+}
