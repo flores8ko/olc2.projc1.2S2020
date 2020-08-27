82e068d53f395d676c530a80fa2fd6500993ef0e
Fernando Flores <nbafernandoflores8@gmail.com>
2020-08-27 16:14:31 -0600

    Relational Operations Functions imports add

---
+++ src/utils/RelationalOperationsFunctions.ts
@@ -0,0 +1,5 @@
+import {Cntnr} from "./Cntnr";
+import {Reference} from "./Reference";
+import {SemanticException} from "./Utils";
+import {BOOLEAN, NAN, NULL, NUMBER, STRING, UNDEFINED} from "./PrimitiveTypoContainer";
+
