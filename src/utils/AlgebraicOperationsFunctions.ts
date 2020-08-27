2850aa9c9c663ced4929335d41486551972aea7b
Fernando Flores <nbafernandoflores8@gmail.com>
2020-08-27 16:42:17 -0600

    fix: Sum Binary function, throw error fixed

--- src/utils/AlgebraicOperationsFunctions.ts
+++ src/utils/AlgebraicOperationsFunctions.ts
@@ -23,14 +23,9 @@ export function Suma(lf: Cntnr, rt: Cntnr): Cntnr {
                         return new NUMBER((lf as NUMBER).getValue() + (rt as BOOLEAN).getValueNumber());
                     case rt instanceof STRING:
                         return new STRING((lf as NUMBER).getValue() + (rt as STRING).getValue());
-                    // case rt instanceof UNDEFINED:
-                    //     return new NAN();
-                    // case rt instanceof NULL:
-                    //     return new NUMBER((lf as NUMBER).getValue());
-                    // case rt instanceof NAN:
-                    //     return new NAN();
+                    default:
+                        throw new Error();
                 }
-                break;
             case lf instanceof BOOLEAN:
                 switch (true) {
                     case rt instanceof NUMBER:
@@ -39,14 +34,9 @@ export function Suma(lf: Cntnr, rt: Cntnr): Cntnr {
                         return new NUMBER((lf as BOOLEAN).getValueNumber() + (rt as BOOLEAN).getValueNumber());
                     case rt instanceof STRING:
                         return new STRING((lf as BOOLEAN).getValue() + (rt as STRING).getValue());
-                    // case rt instanceof UNDEFINED:
-                    //     return new NAN();
-                    // case rt instanceof NULL:
-                    //     return new BOOLEAN((lf as BOOLEAN).getValue());
-                    // case rt instanceof NAN:
-                    //     return new NAN();
+                    default:
+                        throw new Error();
                 }
-                break;
             case lf instanceof STRING:
                 switch (true) {
                     case rt instanceof NUMBER:
@@ -59,58 +49,23 @@ export function Suma(lf: Cntnr, rt: Cntnr): Cntnr {
                         return new STRING((lf as STRING).getValue() + (rt as UNDEFINED));
                     case rt instanceof NULL:
                         return new STRING((lf as STRING).getValue() + (rt as NULL));
-                    // case rt instanceof NAN:
-                    //     return new STRING((lf as STRING).getValue() + (rt as NAN));
+                    default:
+                        throw new Error();
                 }
-                break;
             case lf instanceof UNDEFINED:
                 switch (true) {
-                    // case rt instanceof NUMBER:
-                    //     return new NAN();
-                    // case rt instanceof BOOLEAN:
-                    //     return new NAN();
                     case rt instanceof STRING:
                         return new STRING((lf as UNDEFINED) + (rt as STRING).getValue());
-                    // case rt instanceof UNDEFINED:
-                    //     return new NAN();
-                    // case rt instanceof NULL:
-                    //     return new NAN();
-                    // case rt instanceof NAN:
-                    //     return new NAN();
+                    default:
+                        throw new Error();
                 }
-                break;
             case lf instanceof NULL:
                 switch (true) {
-                    // case rt instanceof NUMBER:
-                    //     return new NUMBER((rt as NUMBER).getValue());
-                    // case rt instanceof BOOLEAN:
-                    //     return new NUMBER((rt as BOOLEAN).getValueNumber());
                     case rt instanceof STRING:
                         return new STRING((lf as NULL) + (rt as STRING).getValue());
-                    // case rt instanceof UNDEFINED:
-                    //     return new NAN();
-                    // case rt instanceof NULL:
-                    //     return new NUMBER(0);
-                    // case rt instanceof NAN:
-                    //     return new NAN();
+                    default:
+                        throw new Error();
                 }
-                break;
-            case lf instanceof NAN:
-                switch (true) {
-                    // case rt instanceof NUMBER:
-                    //     return new NAN();
-                    // case rt instanceof BOOLEAN:
-                    //     return new NAN();
-                    // case rt instanceof STRING:
-                    //     return new STRING((lf as NAN) + (rt as STRING).getValue());
-                    // case rt instanceof UNDEFINED:
-                    //     return new NAN();
-                    // case rt instanceof NULL:
-                    //     return new NAN();
-                    // case rt instanceof NAN:
-                    //     return new NAN();
-                }
-                break;
             default:
                 throw new Error();
         }
