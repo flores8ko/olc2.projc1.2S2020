d371f59717cb1e539dd2923acde05590f475f6d5
Fernando Flores <nbafernandoflores8@gmail.com>
2020-08-31 14:32:14 -0600

    feat: Create Array Var Node Structure add

---
+++ src/nodes/CreateArrVarNode.ts
@@ -0,0 +1,40 @@
+import {Op} from "../utils/Op";
+import {Envmnt} from "../utils/Envmnt";
+import {Cntnr} from "../utils/Cntnr";
+import {Reference} from "../utils/Reference";
+import {SemanticException} from "../utils/Utils";
+import {ARRAY, NUMBER, UNDEFINED} from "../utils/PrimitiveTypoContainer";
+
+export class CreateArrVarNode extends Op{
+    private readonly id: Op;
+    private readonly index: Op;
+
+    constructor(id: Op, index: Op) {
+        super();
+        this.id = id;
+        this.index = index;
+    }
+
+    GO(env: Envmnt): object {
+        let idRef = this.id.Exe(env) as Cntnr;
+        if (!(idRef instanceof Reference)) {
+            throw new SemanticException(`Llamada a Arreglo ${idRef} no definido.`);
+        }
+
+        let index = this.index.Exe(env) as Cntnr;
+        if (index instanceof Reference) {
+            index = (index as Reference).getValue();
+        }
+        if (!(index instanceof NUMBER)) {
+            throw new SemanticException("El indice para accesar debe ser de tipo NUMBER");
+        }
+
+        let ref = (idRef as Reference).getValue();
+
+        if (!(ref instanceof ARRAY)) {
+            return new UNDEFINED();
+        }
+
+        return (ref as ARRAY).getValue((index as NUMBER).getValue());
+    }
+}
