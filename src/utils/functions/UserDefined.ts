06c1a4fb75e2b31d05dfbaf4b7464474fad8de6d
Fernando Flores <nbafernandoflores8@gmail.com>
2020-08-31 20:06:14 -0600

    feat: User Defined structure add

---
+++ src/utils/functions/UserDefined.ts
@@ -0,0 +1,33 @@
+import {FunctionRepresent} from "./FunctionRepresent";
+import {Op} from "../Op";
+import {Envmnt} from "../Envmnt";
+import {Cntnr} from "../Cntnr";
+import {Reference} from "../Reference";
+
+export class UserDefined extends FunctionRepresent {
+    private readonly src: Array<Op>;
+    private readonly params: Array<Op>;
+
+    constructor(src: Array<Op>, params: Array<Op>) {
+        super();
+        this.src = src;
+        this.params = params;
+    }
+
+    public getSrc(): Array<Op> {
+        return this.src;
+    }
+
+    EXE(env0: Envmnt, args: Array<Cntnr>): Cntnr {
+        let env = new Envmnt(env0, this.src);
+        const references: Array<Reference> = new Array<Reference>();
+        for (let param of this.params) {
+            references.push(param.Exe(env) as Reference);
+        }
+
+        for (let i = 0; i < args.length && i < references.length; i++) {
+            references[i].setValue(args[i]);
+        }
+        return env.GO_ALL();
+    }
+}
