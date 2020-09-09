2342b6806b7a543b7985496620880f49cf6f544b
Fernando Flores <nbafernandoflores8@gmail.com>
2020-09-09 11:24:43 -0600

    feat: For Of Node structure add

--- src/nodes/ForOfNode.ts
+++ src/nodes/ForOfNode.ts
@@ -1,10 +1,10 @@
 import {Op} from "../utils/Op";
 import {Envmnt} from "../utils/Envmnt";
 import {Reference} from "../utils/Reference";
-import {ARRAY, NUMBER, STRING} from "../utils/PrimitiveTypoContainer";
+import {ARRAY} from "../utils/PrimitiveTypoContainer";
 import {FindVar, SemanticException} from "../utils/Utils";
 
-export class ForInNode extends Op {
+export class ForOfNode extends Op {
     private readonly controlVar: string;
     private readonly newControlVar: boolean;
     private readonly array: Op;
@@ -24,7 +24,7 @@ export class ForInNode extends Op {
             array = (array as Reference).getValue();
         }
         if (!(array instanceof ARRAY)) {
-            throw new SemanticException("Se esperaba una referencia a un arreglo en ciclo For In");
+            throw new SemanticException("Se esperaba una referncia a un arreglo en ciclo For Of")
         }
 
         const env0 = new Envmnt(env, this.sentences);
@@ -32,8 +32,12 @@ export class ForInNode extends Op {
             env0.AddProperty(this.controlVar, new Reference());
         }
 
-        for(let index in (array as ARRAY).getValueList()){
-            (FindVar(env0, this.controlVar) as Reference).setValue(new STRING(index));
+        for (let element of (array as ARRAY).getValueList()) {
+            let val = element;
+            if (val instanceof Reference) {
+                val = (val as Reference).getValue();
+            }
+            (FindVar(env0, this.controlVar) as Reference).setValue(val);
             env0.GO_ALL();
         }
         return undefined;
