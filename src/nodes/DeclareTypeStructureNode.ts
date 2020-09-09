183f7d52ff8a06837633d4c3ed9ab75c9dcc8361
Fernando Flores <nbafernandoflores8@gmail.com>
2020-09-09 15:10:19 -0600

    feat: Declare Type structure node add

---
+++ src/nodes/DeclareTypeStructureNode.ts
@@ -0,0 +1,20 @@
+import {Op} from "../utils/Op";
+import {Envmnt} from "../utils/Envmnt";
+import {ObjectsStructures, ObjectStructure} from "../utils/Utils";
+
+export class DeclareTypeStructureNode extends Op{
+    private readonly name: string;
+    private readonly properties: Map<string, string>;
+
+    constructor(name: string, properties: Map<string, string>) {
+        super();
+        this.name = name;
+        this.properties = properties;
+    }
+
+    GO(env: Envmnt): object {
+        const structure = new ObjectStructure(this.properties);
+        ObjectsStructures.objects.set(name, structure);
+        return undefined;
+    }
+}
